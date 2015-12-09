68a321f45c1062e570bf259990c493d54c841952
Amit Netanel <netanel.amit@gmail.com>
2015-12-09 20:22:19 +0200

    more css features

--- main.js
+++ main.js
@@ -5,7 +5,19 @@ btn.appendChild(t);
 //Appending to DOM 
 document.body.appendChild(btn);
 
+// should only work when the right configuration flag has been turned on.
+function add_dash_to_end_of_line(){
+	console.log('blat');
+	$("span[class^=word]").each(function(index, element){ 
+		$(element).text($(element).text() + "/");
+	});
+}
+
 $(document).ready(function() {
-  // $(".mw-content-rtl p").lettering('words').children('span').lettering();
-  $("p").lettering('words').children('span').lettering();
+  // $("p, li").lettering('lines').children('span').lettering('words').children('span').lettering();
+  $("p, li").lettering('lines').children('span').lettering('words').children('span').lettering();
+  // add_dash_to_end_of_line();
 });
+
+
+
