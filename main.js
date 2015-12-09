054847afd8b03cf4fe0a4c34d6568a2e90d17fc7
Amit Netanel <netanel.amit@gmail.com>
2015-12-09 15:04:53 +0200

    added some logic

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ var t = document.createTextNode("CLICK ME");
 btn.appendChild(t);
 //Appending to DOM 
 document.body.appendChild(btn);
+
+console.log('LIAT');
+$(document).ready(function() {
+  $(".mw-content-rtl p").lettering('words');
+  console.log('AMOT');
+});
