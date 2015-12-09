23c0c5eb88ee7c113b38ce458d577565e64f395f
Vladimir Gritsenko <vladimir.gritsenko@gmail.com>
2015-12-09 12:49:43 +0200

    Very basic HTML editing sample, via DOM, taken from here: http://stackoverflow.com/questions/14068879/modify-html-of-loaded-pages-using-chrome-extensions

---
+++ main.js
@@ -0,0 +1,6 @@
+//Creating Elements
+var btn = document.createElement("BUTTON")
+var t = document.createTextNode("CLICK ME");
+btn.appendChild(t);
+//Appending to DOM 
+document.body.appendChild(btn);
