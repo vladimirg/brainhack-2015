a06a08b8c107781b9fb158c0af79a3496683f27e
Eyal Bukchin <eyalb@candirusecurity.com>
2015-12-10 14:24:56 +0200

    Fix background color, change font-weight to bold.

--- main.js
+++ main.js
@@ -43,45 +43,43 @@ $(document).ready(function() {
             stylesheet.insertRule(`span[class^=char]:last-child{color: ${items.textColor}}`, settings_enumerator);
             settings_enumerator++;
           }
-          if (items.shouldChangeBackgroundColor){
-            stylesheet.insertRule(`span[class^=char] { background-color: ${items.backgroundColor}; }`, settings_enumerator); 
-            settings_enumerator++;
-          }
           if (items.shouldHighlightOnHover != "nothing"){
             if (items.shouldVertical){
-              stylesheet.insertRule(`span[class^=word] { opacity: 0.4;}`, settings_enumerator); 
+              stylesheet.insertRule(`span[class^=word] { opacity: 0.2;}`, settings_enumerator);
               settings_enumerator++;
-              stylesheet.insertRule(`span[class^=word]:hover { opacity: 1;}`, settings_enumerator); 
+              stylesheet.insertRule(`span[class^=word]:hover { opacity: 1;}`, settings_enumerator);
+              stylesheet.insertRule(`span[class^=word]:hover { font-weight: bold;}`, settings_enumerator);
               settings_enumerator++;
             }else{
-              stylesheet.insertRule(`span[class^=${items.shouldHighlightOnHover}] { opacity: 0.4;}`, settings_enumerator);
+              stylesheet.insertRule(`span[class^=${items.shouldHighlightOnHover}] { opacity: 0.2;}`, settings_enumerator);
               settings_enumerator++;
               stylesheet.insertRule(`span[class^=${items.shouldHighlightOnHover}]:hover { opacity: 1;}`, settings_enumerator);
+              stylesheet.insertRule(`span[class^=${items.shouldHighlightOnHover}]:hover { font-weight: bold;}`, settings_enumerator);
               settings_enumerator++;
             }
           }
           if (items.shouldVertical){
-            stylesheet.insertRule(`span[class^=word] { margin-top: 3px; margin-bottom: 3px; display: block;}`, settings_enumerator); 
+            stylesheet.insertRule(`span[class^=word] { margin-top: 3px; margin-bottom: 3px; display: block;}`, settings_enumerator);
             settings_enumerator++;
           }
           if (items.shouldPaintLastLetter){
-              stylesheet.insertRule(`span[class^=char]:last-child { color: red;}`, settings_enumerator); 
+              stylesheet.insertRule(`span[class^=char]:last-child { color: red;}`, settings_enumerator);
               settings_enumerator++;
           }
           if (items.shouldAnimateLastLetter){
-              stylesheet.insertRule(`span[class^=char]:last-child { animation: myfirst 2s infinite; -moz-animation:myfirst 2s infinite; -webkit-animation: myfirst 2s infinite;}`, settings_enumerator); 
+              stylesheet.insertRule(`span[class^=char]:last-child { animation: myfirst 2s infinite; -moz-animation:myfirst 2s infinite; -webkit-animation: myfirst 2s infinite;}`, settings_enumerator);
               settings_enumerator++;
           }
           if (items.shouldPaintFirstLetter){
-              stylesheet.insertRule(`span[class^=char]:first-child { color: red;}`, settings_enumerator); 
+              stylesheet.insertRule(`span[class^=char]:first-child { color: red;}`, settings_enumerator);
               settings_enumerator++;
           }
           if (items.shouldAnimateFirstLetter){
-              stylesheet.insertRule(`span[class^=char]:first-child { animation: myfirst 2s infinite; -moz-animation:myfirst 2s infinite; -webkit-animation: myfirst 2s infinite;}`, settings_enumerator); 
+              stylesheet.insertRule(`span[class^=char]:first-child { animation: myfirst 2s infinite; -moz-animation:myfirst 2s infinite; -webkit-animation: myfirst 2s infinite;}`, settings_enumerator);
               settings_enumerator++;
           }
           if (items.shouldLetterSpacing){
-              stylesheet.insertRule(`span[class^=word] { letter-spacing: ${items.letterSpacing}px; }`, settings_enumerator); 
+              stylesheet.insertRule(`span[class^=word] { letter-spacing: ${items.letterSpacing}px; }`, settings_enumerator);
               settings_enumerator++;
           }
 
@@ -92,38 +90,46 @@ $(document).ready(function() {
             // add_dash_to_end_of_line();
           }
 
-        select = function(element){
-          $("span.selected").removeClass("selected");
-          $(element).addClass("selected").focus();
-        };
-        next_word = function(element) {
-          var $words = $("span[class^=word]");
-          return $words.eq($words.index(element) + 1);
-        };
-        prev_word = function(element) {
-          var $words = $("span[class^=word]");
-          return $words.eq($words.index(element) - 1);
-        };
-        $("span[class^=word]").click(function() {
-          select(this);
-        });
-        var interval = null;
-        $("body").keydown(function(e) {
-          if (e.keyCode == 79) {
-            select(prev_word($("span.selected")));
-          } else if (e.keyCode == 80) {
-            select(next_word($("span.selected")));
-          } else if (e.keyCode == 77) {
-            if (interval) {
-              window.clearInterval(interval);
-              interval = null;
-            } else {
-              interval = setInterval(function () {
-                select(next_word($("span.selected")))
-              }, 2000);
+          if (items.shouldChangeBackgroundColor){
+            element = $("span[class^=char]");
+            while(element.length != 0){
+              element.css("background-color", items.backgroundColor)
+              element = element.parent();
             }
           }
-        });
+
+          select = function(element){
+            $("span.selected").removeClass("selected");
+            $(element).addClass("selected").focus();
+          };
+          next_word = function(element) {
+            var $words = $("span[class^=word]");
+            return $words.eq($words.index(element) + 1);
+          };
+          prev_word = function(element) {
+            var $words = $("span[class^=word]");
+            return $words.eq($words.index(element) - 1);
+          };
+          $("span[class^=word]").click(function() {
+            select(this);
+          });
+          var interval = null;
+          $("body").keydown(function(e) {
+            if (e.keyCode == 79) {
+              select(prev_word($("span.selected")));
+            } else if (e.keyCode == 80) {
+              select(next_word($("span.selected")));
+            } else if (e.keyCode == 77) {
+              if (interval) {
+                window.clearInterval(interval);
+                interval = null;
+              } else {
+                interval = setInterval(function () {
+                  select(next_word($("span.selected")))
+                }, 2000);
+              }
+            }
+          });
 
         });
     });
