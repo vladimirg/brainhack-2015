e3fc08411f2385a533b28b7228267266b1344d42
Amit Netanel <netanel.amit@gmail.com>
2015-12-10 13:10:25 +0200

    updated icons

--- main.js
+++ main.js
@@ -120,7 +120,7 @@ $(document).ready(function() {
             } else {
               interval = setInterval(function () {
                 select(next_word($("span.selected")))
-              }, 2000);
+              }, 500);
             }
           }
         });
